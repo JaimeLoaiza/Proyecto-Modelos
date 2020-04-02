d16593f2fd08cac97dcec9e32f48d9d9139f4a50
JaimeLoaiza <jaimeloaiza@hotmail.es>
2020-04-01 20:11:29 -0500

    Proyecto1-Modelos

---
+++ Modelos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Modelos
+{
+    public class Modelos
+    {
+        public int codigo { get; set; }
+        public string nombre { get; set; }
+        public int edad { get; set; }
+        public string sexo { get; set; }
+        public double sueldo { get; set; }
+    }
+}
