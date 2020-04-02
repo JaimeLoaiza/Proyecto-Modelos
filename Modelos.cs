e01c919fb384e02db4bc74c3992d5009e793ebdf
JaimeLoaiza <jaimeloaiza@hotmail.es>
2020-04-01 22:31:51 -0500

    Actualizar

--- Modelos.cs
+++ Modelos.cs
@@ -10,6 +10,7 @@ namespace Proyecto_Modelos
     {
         public int codigo { get; set; }
         public string nombre { get; set; }
+        public string apellido { get; set; }
         public int edad { get; set; }
         public string sexo { get; set; }
         public double sueldo { get; set; }
